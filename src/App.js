c2e80fcb7d16ffff662ac9cf979a0a0a3b9ed2ed
iiishokoiii <tobby_36@yahoo.co.jp>
2021-12-06 20:45:32 +0900

    lesson1 演習

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <h1 className="header">ToDo App</h1>
+      <p>TODOが追加できるよ</p>
+    </>
+  );
+}
+
+function List() {
+  const list = [
+    {
+      title:"Reactのインストール",
+      checked:true
+    },
+    {
+      title:"JSXを学ぶ",
+      checked:true
+    },
+    {
+      title:"コンポーネントの分離",
+      checked:false
+    },
+    {
+      title:"演習",
+      checked:false
+    }
+  ]
+  return (
+    <ul className="list">
+      {list.map((item, i) => (
+        <li
+          className={item.checked ? "checked" : ""}
+          key={i}
+        >{item.title}</li>
+      ))}
+    </ul>
+  )
+}
+
+function Form() {
+  return (
+    <div className="form">
+      <label htmlFor="text">追加</label>
+      <input type="text" id="text" defaultValue="ダミー" />
+      <input type="button" value="追加" />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <List/>
+      <Form/>
+    </>
   );
 }
 
