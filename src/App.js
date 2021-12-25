c1387481bbb31cc71c9951dd3e63b2cde560f626
iiishokoiii <tobby_36@yahoo.co.jp>
2021-12-25 15:27:42 +0900

    演習2 Advanced:編集機能を追加

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState, Fragment } from 'react'
 
 
 function Header() {
@@ -11,19 +11,37 @@ function Header() {
   );
 }
 
-function List({ list }) {
+// list の内容を表示する
+// アイテムの isEditing が true の場合は編集モードで表示する
+// htmlにタグを出力したくない場合に、 React.Fragment を使用する
+function List({ list, editItem, startEditihg, finishEditing }) {
+  console.log(list)
   return (
     <ul className="list">
       {list.map((item, i) => (
-        <li
-          className={item.checked ? "checked" : ""}
-          key={i}
-        >{item.title}</li>
+        <React.Fragment key={i}>
+          {!item.isEditing
+            ?
+            <>
+              <li
+                className={item.checked ? "checked" : ""}
+              >{item.title}</li>
+              <button
+                onClick={() => startEditihg(i)}
+              >編集</button>
+            </>
+            :
+            <li>
+              <Editor editItem={editItem} finishEditing={finishEditing} itemTitle={item.title} index={i} />
+            </li>
+          }
+        </React.Fragment>
       ))}
     </ul>
   )
 }
 
+// 追加ボタンを押すとアイテムを追加できる
 function Form({addItem}) {
   const [tmpText, changeTmpText] = useState('') // 子コンポーネント内でのみ使用
   const [errorText, changeErrorText] = useState('')
@@ -56,32 +74,102 @@ function Form({addItem}) {
   )
 }
 
+//アイテム単位の編集モード、確定とキャンセルができる
+//editItem, finishEditing: function
+//itemTitle: 編集前のタイトル初期値
+//index: 編集するアイテムのインデックス
+function Editor({editItem, finishEditing, itemTitle, index}) {
+  const [tmpText, changeTmpText] = useState(itemTitle) // 子コンポーネント内でのみ使用
+
+  return (
+    <div className="form">
+      <input
+        type="text"
+        id="text"
+        value={tmpText}
+        onChange={e => { changeTmpText(e.currentTarget.value) }}
+      />
+      <button
+        onClick={() => editItem(tmpText, index)}
+      >change</button>
+      <button
+        onClick={() => {
+          finishEditing(index)
+          changeTmpText(itemTitle)
+        }}
+      >cancel</button>
+    </div>
+  )
+}
+
 function App() {
   const [list, changeList]  = useState([
     {
       title:"Reactのインストール",
-      checked:true
+      checked: true,
+      isEditing: false
     },
     {
       title:"JSXを学ぶ",
-      checked:true
+      checked:true,
+      isEditing: false
     },
     {
       title:"コンポーネントの分離",
-      checked:false
+      checked:false,
+      isEditing: false
     },
     {
       title:"演習",
-      checked:false
+      checked:false,
+      isEditing: false
     }
   ])
 
+  //指定したtitleのアイテムを追加する
   const addItem = (title) => {
     const newItem = {
       title: title,
-      checked: false
+      checked: false,
+      isEditing: false
     }
     changeList([...list, newItem])
+  }
+  
+  //インデックスで指定したアイテムの title の値を変更する
+  const editItem = (title, editIndex) => {
+    const tmpList = list.map((item, idx) => {
+      if (idx === editIndex) {
+        return {...item, title: title, isEditing: false}
+      } else {
+        return item
+      }
+    })
+    changeList(tmpList)
+  }
+
+  //インデックスで指定したアイテムの iseEditing の値をTrueに変更する
+  const startEditing = (editIndex) => {
+    const tmpList = list.map((item, idx) => {
+      if (idx === editIndex) {
+        return {...item, isEditing: true}
+      } else {
+        return item
+      }
+    })
+    changeList(tmpList)
+  }
+
+  //インデックスで指定したアイテムの iseEditing の値をFalseに変更する
+  const finishEditing = (editIndex) => {
+    const tmpList = list.map((item, idx) => {
+      if (idx === editIndex) {
+        return {...item, isEditing: false}
+      } else {
+        return item
+      }
+    })
+    changeList(tmpList)
   }
 
   return (
@@ -89,6 +177,9 @@ function App() {
       <Header/>
       <List
         list={list}
+        editItem={editItem}
+        startEditihg={startEditing}
+        finishEditing={finishEditing}
         />
       <Form
         addItem={addItem}
